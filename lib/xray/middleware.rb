bdad0f4b08747ea60aaf5d0c9101dee1e31a51ed
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-05-10 14:03:33 -0500

    Respect application config when generating javascript includes
    Specifically, use the proper path when an application has set

--- lib/xray/middleware.rb
+++ lib/xray/middleware.rb
@@ -61,9 +61,12 @@ module Xray
       #   <script src="/assets/jquery-min.js"></script>
       #   <script src="/assets/jquery.min.1.9.1.js"></script>
       html.sub!(/<script[^>]+\/#{after_script_name}([-.]{1}[\d\.]+)?([-.]{1}min)?\.js[^>]+><\/script>/) do
-        h = ActionController::Base.helpers
-        "#{$~}\n" + h.javascript_include_tag(script_name)
+        "#{$~}\n" + view_context.javascript_include_tag(script_name)
       end
+    end
+
+    def view_context
+      @view_context ||= Rails.application.assets.context_class.new nil, nil, nil
     end
 
     def should_inject_xray?(status, headers, response)
