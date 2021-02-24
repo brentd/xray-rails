44a878effacfc03fc2f68612302b68011ad4dde8
Tyler Rick <tyler@tylerrick.com>
2021-02-24 09:50:27 -0800

    In CI, we can't open the .html files saved by capybara-screenshot, so puts page.html instead so we can figure out why it failed

--- spec/support/capybara_screenshot.rb
+++ spec/support/capybara_screenshot.rb
@@ -9,6 +9,9 @@ RSpec.configure do |config|
     # RSpec 3 no longer defines `example`, but passes the example as block argument instead
     example = config.respond_to?(:expose_current_running_example_as) ? example_from_block_arg : self.example
 
+    if example.exception && ENV['CI']
+      puts page.html
+    end
     Capybara::Screenshot::RSpec.after_failed_example(example)
   end
 end
